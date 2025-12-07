5de830f7b7082a79b446668f8abbcbb1babd6966
Makima32 <omartorresyuste@gmail.com>
2025-12-07 22:10:09 +0000

    Corrige la ruta de importación del archivo CSS en ModificarPlato

--- Frontend/pedidos/src/component/AdminComponents/modificar/editarPlato/modificarPlato.jsx
+++ Frontend/pedidos/src/component/AdminComponents/modificar/editarPlato/modificarPlato.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "../../formularios/formularios.css"; 
+import "../../Formularios/formularios.css"; 
 
 function ModificarPlato() {
     const [platos, setPlatos] = useState([]);
