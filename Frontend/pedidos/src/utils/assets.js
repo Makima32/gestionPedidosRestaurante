eaed2a7db8acf7c43da61fe59b95e5b3ffe12b86
Makima32 <omartorresyuste@gmail.com>
2026-02-21 20:52:28 +0000

    cambios y agregacion de pagina pedidos

---
+++ Frontend/pedidos/src/utils/assets.js
@@ -0,0 +1,18 @@
+
+export const IMAGENES = {
+
+
+    //Home images
+    BANNERS: {
+
+            PC: "/assets/banners/banner_pc.webp",      
+            TABLET: "/assets/banners/banner_tablet.webp", 
+            MOBILE: "/assets/banners/banner_mobile.webp"  
+    },
+    ICONOS: {
+        CARRITO: "/assets/icons/cart.svg",
+        PLUS: "/assets/icons/plus.svg"
+    },
+
+
+};
